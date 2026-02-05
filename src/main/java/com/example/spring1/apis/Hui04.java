5cac457f18441807793dcea09ebc319f4f4b9812
ChingHui418 <a0985218998@gmail.com>
2026-02-05 13:58:55 +0800

    20260205-1400/Bike

---
+++ src/main/java/com/example/spring1/apis/Hui04.java
@@ -0,0 +1,35 @@
+package com.example.spring1.apis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.spring1.utils.Bike;
+import com.example.spring1.utils.Bike2;
+
+@RestController
+@RequestMapping("/hui04")
+public class Hui04 {
+	@Autowired
+	private Bike bike1;
+
+	@Autowired
+	private Bike bike2;
+
+	@Autowired
+	@Qualifier("bike1")
+	private Bike bike3;
+	
+	@Autowired
+	private Bike2 mybike;
+	
+	@RequestMapping("/test1")
+	public void test1() {
+		System.out.println("Hui04:test1()");
+		bike1.upSpeed();
+		bike2.upSpeed();
+		bike3.upSpeed();
+		mybike.upSpeed();
+	}
+}
